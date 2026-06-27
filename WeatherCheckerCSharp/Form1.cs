0142c69246bea1322471cba22737276ffe39a527
IchikaCoding <IchikaCoding@users.noreply.github.com>
2026-06-27 21:59:05 +0900

    fix: btnSearch_Click()のミスを仮修正

--- WeatherCheckerCSharp/Form1.cs
+++ WeatherCheckerCSharp/Form1.cs
@@ -29,34 +29,50 @@ namespace WeatherCheckerCSharp
         private async void btnSearch_Click(object sender, EventArgs e)
         {
             // JSON文字列がない、、、
-            string cityName = txtCity.Text;
-            cityName.Trim();
+            // この処理結果を代入しておく必要がある
+            string cityName = txtCity.Text.Trim();
+
             // GeoResponseを受ける
             // URLで都市名で検索する
             // URLの作り方がわからない。非同期処理がGood
-            string url = "https://geocoding-api.open-meteo.com/v1/search/&?name={cityName}&?count=1&language=ja&format=json";
+            // 変数名が変数名っぽく光っているかどうかを確認しよう！
+            // TODO: どうしてURLを一括で書かないの？
+                //　Uri.EscapeDataString(cityName)はどうして使うの？
+            string geoUrl = $"https://geocoding-api.open-meteo.com/v1/search" + $"?name={EscapeDataString(cityName)}&count=1&language=ja&format=json";
             // EscapeDataStringでURLを安全な文字列に修正してくれうらしんだけど、間違っているかも、、、、
-            string geoJson = await http.GetStringAsync(EscapeDataString(url));
-
-            // 都市名が見つからなかった場合はここで早期リターン
-            if(geoJson is null)
-            {
-                lblStatus.Text =$"「{cityName}」が見つかりません";
-                return;
-            }
+            // TODO: EscapeDataString()はここにいるのか？いらないのかい？
+            string geoJson = await http.GetStringAsync(geoUrl);
             txtRaw.Text = geoJson;
 
-           // デシリアライズするとrecordの型にはめる事が可能（？）
+            // デシリアライズするとrecordの型にはめる事が可能（？）
             GeoResponse? geoString = JsonSerializer.Deserialize<GeoResponse>(geoJson);
 
             // 緯度経度を取得する
+            // TODO: これ間違っているらしい。
+            // GeoResult? hit = geo?.Results?.FirstOrDefault();らしいよ
+            // GeoResponseに入れたデータはレコードの型になっている。
+            // そのResultsにアクセスすると、List<GeoResult>が取れる。
             List<GeoResult> geoResults = geoString.Results;
-            GeoResult geoFirstItem = geoResults[1];
+
+            // Listの最初の要素は0だよ
+            GeoResult geoFirstItem = geoResults[0];
+
+            // 都市名が見つからなかった場合はここで早期リターン
+            // TODO: 取得出来なかった判定はどうやってやるの？
+            // GeoResultのNameプロパティがnullだったら、という条件じゃだめ？
+            if (geoFirstItem is null)
+            {
+                lblStatus.Text = $"「{cityName}」が見つかりません";
+                return;
+            }
+            
             double latitude = geoFirstItem.Latitude;
             double longitude = geoFirstItem.Longitude;
             
             // 3日分取得
-            string forecastUrl = $"https://api.open-meteo.com/v1/forecast/?latitude={latitude}/?longitude={longitude}/?daily=weather_code,temperature_2m_max,temperature_2m_min,precipitation_probability_max,timezone=Asia%2FTokyo,forecast_days=3";
+            // URLのパラメーター部分は最初?でその後は＆で続ける
+            // TODO: ＆とカンマの違いと、カンマの位置と足し算にする場所が不明
+            string forecastUrl = $"https://api.open-meteo.com/v1/forecast" + $"?latitude={latitude}&longitude={longitude}" + "&daily=weather_code,temperature_2m_max,temperature_2m_min,precipitation_probability_max" + "&timezone=Asia%2FTokyo&forecast_days=3";
             string forecastJson = await http.GetStringAsync(forecastUrl);
             // ForecastResponse型のデータにする
             ForecastResponse? forecastResponse = JsonSerializer.Deserialize<ForecastResponse>(forecastJson);
